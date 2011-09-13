07d2ea8326106ed046663838e1883f8de8522931
Olaf Lüke <olaf@tinkerforge.com>
2011-09-13 19:04:38 +0200

    add java bindings and examples

---
+++ software/bindings/java/ExampleStackStatus.java
@@ -0,0 +1,28 @@
+import com.tinkerforge.BrickMaster;
+import com.tinkerforge.IPConnection;
+
+public class ExampleStackStatus {
+	private static final String host = new String("localhost");
+	private static final int port = 4223;
+	private static final String UID = new String("apaYPbNgTw3"); // Change to your UID
+	
+	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
+	//       might normally want to catch are described in the commnents below
+	public static void main(String args[]) throws Exception {
+		IPConnection ipcon = new IPConnection(host, port); // Create connection to brickd (Can throw IOException)
+
+		BrickMaster master = new BrickMaster(UID); // Create device object
+		ipcon.addDevice(master); // Add device to ip connection (Can throw IPConnection.TimeoutException)
+		// Don't use device before it is added to a connection
+		
+		// Get voltage and current from stack (in mV/mA)
+		int voltage = master.getStackVoltage();
+		int current = master.getStackCurrent();
+
+		System.out.println("Stack Voltage: " + voltage/1000.0 + " V");
+		System.out.println("Stack Current: " + current/1000.0 + " A");
+		
+		System.out.println("Press ctrl+c to exit");
+		ipcon.joinThread();
+	}
+}
