b15a354828a1888bf04b39a16a14deb99efec2de
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 17:54:01 +0100

    Make PHP examples Protocol 2.0 compatible

--- software/examples/php/ExampleStackStatus.php
+++ software/examples/php/ExampleStackStatus.php
@@ -10,11 +10,11 @@ $host = 'localhost';
 $port = 4223;
 $uid = 'a4GeP9ZpQFT'; // Change to your UID
 
-$ipcon = new IPConnection($host, $port); // Create IP connection to brickd
-$master = new BrickMaster($uid); // Create device object
+$ipcon = new IPConnection(); // Create IP connection
+$master = new BrickMaster($uid, $ipcon); // Create device object
 
-$ipcon->addDevice($master); // Add device to IP connection
-// Don't use device before it is added to a connection
+$ipcon->connect($host, $port); // Connect to brickd
+// Don't use device before ipcon is connected
 
 // Get voltage and current from stack (in mV/mA)
 $voltage = $master->getStackVoltage();
@@ -25,6 +25,5 @@ echo "Stack Current: " . $current / 1000.0 . " A\n";
 
 echo "Press key to exit\n";
 fgetc(fopen('php://stdin', 'r'));
-$ipcon->destroy();
 
 ?>
