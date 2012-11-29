a34f384d71d22599290266d3d228b52863f9d197
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 11:54:37 +0100

    Make Ruby examples Protocol 2.0 compatible

--- software/examples/ruby/example_stack_status.rb
+++ software/examples/ruby/example_stack_status.rb
@@ -10,10 +10,11 @@ HOST = 'localhost'
 PORT = 4223
 UID = 'a4GeP9ZpQFT' # Change to your UID
 
-ipcon = IPConnection.new HOST, PORT # Create IP connection to brickd
-master = BrickMaster.new UID # Create device object
-ipcon.add_device master # Add device to IP connection
-# Don't use device before it is added to a connection
+ipcon = IPConnection.new # Create IP connection
+master = BrickMaster.new UID, ipcon # Create device object
+
+ipcon.connect HOST, PORT # Connect to brickd
+# Don't use device before ipcon is connected
 
 # Get voltage and current from stack (in mV/mA)
 voltage = master.get_stack_voltage
@@ -24,4 +25,3 @@ puts "Stack Current: #{current/1000.0} A"
 
 puts 'Press key to exit'
 $stdin.gets
-ipcon.destroy
