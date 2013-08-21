aebd50c622de94eff0f7ba3437e50f61ec867a30
Matthias Bolte <matthias@tinkerforge.com>
2013-08-21 17:08:30 +0200

    windows: Don't trigger a build in generate_makefile.bat
    Also use normal make instead of a special named one.

--- software/generate_makefile.bat
+++ software/generate_makefile.bat
@@ -1,5 +1,2 @@
-rem windows command shell batch script - generate_makefile.cmd
-cmake -E chdir build/ cmake -G "MinGW Makefiles" -DCMAKE_MAKE_PROGRAM=cs-make -DCMAKE_TOOLCHAIN_FILE=toolchains/arm-none-eabi.cmake ../
-cmake --build build/ --target clean
-cmake --build build/ --use-stderr
-pause
+rem windows command shell batch script
+cmake -E chdir build/ cmake -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=toolchains/arm-none-eabi.cmake ../
