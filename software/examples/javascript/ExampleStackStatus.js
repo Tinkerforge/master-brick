2a7655090d43461202bef33bdcf48fabc0cce66c
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-11 15:26:46 +0100

    javascript: short examples

--- software/examples/javascript/ExampleStackStatus.js
+++ software/examples/javascript/ExampleStackStatus.js
@@ -10,11 +10,9 @@ var master = new BrickMaster(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        if(error === IPConnection.ERROR_ALREADY_CONNECTED) {
-            console.log('Error: Already connected');        
-        }
+        console.log('Error: '+error);        
     }
-);//Connect to brickd
+);// Connect to brickd
 
 // Don't use device before ipcon is connected
 ipcon.on(IPConnection.CALLBACK_CONNECTED,
@@ -25,9 +23,7 @@ ipcon.on(IPConnection.CALLBACK_CONNECTED,
                 console.log('Stack Voltage: '+voltage/1000+' V');
             },
             function(error) {
-                if(error === IPConnection.RESPONSE_TIMED_OUT) {
-                  console.log('Error: The request timed out');
-                }
+                console.log('Error: '+error);
             }
         );
         master.getStackCurrent(
@@ -35,23 +31,17 @@ ipcon.on(IPConnection.CALLBACK_CONNECTED,
                 console.log('Stack Current: '+current/1000+' A');
             },
             function(error) {
-                if(error === IPConnection.RESPONSE_TIMED_OUT) {
-                  console.log('Error: The request timed out');
-                }
+                console.log('Error: '+error);
             }
         );
     }
 );
 
 console.log("Press any key to exit ...");
-process.stdin.on('data', function(data) {
-	    ipcon.disconnect(
-            function(error) {
-                if(error === IPConnection.ERROR_NOT_CONNECTED) {
-                    console.log('Error: Not connected');        
-                }
-            }
-        );
-process.exit(0);
-});
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
 
