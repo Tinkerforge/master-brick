5896ed9ace30c8fba38703a38d9f2364f90ae3ba
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-03-25 11:50:38 +0100

    matlab: adding examples

---
+++ software/examples/matlab/matlab_example_stack_status.m
@@ -0,0 +1,24 @@
+function matlab_example_stack_status
+    import com.tinkerforge.IPConnection;
+    import com.tinkerforge.BrickMaster;
+
+    HOST = 'localhost';
+    PORT = 4223;
+    UID = '68WRjG'; % Change to your UID
+    
+    ipcon = IPConnection(); % Create IP connection
+    master = BrickMaster(UID, ipcon); % Create device object
+
+    ipcon.connect(HOST, PORT); % Connect to brickd
+    % Don't use device before ipcon is connected
+
+    % Get voltage and current from stack (in mV/mA)
+    voltage = master.getStackVoltage();
+    current = master.getStackCurrent();
+
+    fprintf('Stack Voltage: %g V\n', voltage/1000);
+    fprintf('Stack Current: %g A\n', current/1000.0);
+
+    input('\nPress any key to exit...\n', 's');
+    ipcon.disconnect();
+end
---
+++ software/examples/matlab/octave_example_stack_status.m
@@ -0,0 +1,23 @@
+function octave_example_stack_status
+    more off;
+    
+    HOST = "localhost";
+    PORT = 4223;
+    UID = "68WRjG"; % Change to your UID
+
+    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
+    master= java_new("com.tinkerforge.BrickMaster", UID, ipcon); % Create device object
+
+    ipcon.connect(HOST, PORT); % Connect to brickd
+    % Don't use device before ipcon is connected
+
+    % Get voltage and current from stack (in mV/mA)
+    voltage = master.getStackVoltage();
+    current = master.getStackCurrent();
+
+    fprintf("Stack Voltage: %g V\n", voltage/1000);
+    fprintf("Stack Current: %g A\n", current/1000.0);
+
+    input("\nPress any key to exit...\n", "s");
+    ipcon.disconnect();
+end
