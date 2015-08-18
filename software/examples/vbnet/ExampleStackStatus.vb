0f7c1d66bbc20f11381a25c28988f50b1a6d1b54
Matthias Bolte <matthias@tinkerforge.com>
2015-08-18 14:45:21 +0200

    Cleanup VB.NET examples

--- software/examples/vbnet/ExampleStackStatus.vb
+++ software/examples/vbnet/ExampleStackStatus.vb
@@ -3,7 +3,7 @@ Imports Tinkerforge
 Module ExampleStackStatus
     Const HOST As String = "localhost"
     Const PORT As Integer = 4223
-    Const UID As String = "6kpiwr" ' Change to your UID
+    Const UID As String = "XYZ" ' Change to your UID
 
     Sub Main()
         Dim ipcon As New IPConnection() ' Create IP connection
@@ -12,12 +12,13 @@ Module ExampleStackStatus
         ipcon.Connect(HOST, PORT) ' Connect to brickd
         ' Don't use device before ipcon is connected
 
-        ' Get voltage and current from stack (in mV/mA)
-        Dim voltage As Integer = master.GetStackVoltage()
-        Dim current As Integer = master.GetStackCurrent()
+        ' Get current stack voltage (unit is mV)
+        Dim stackVoltage As Integer = master.GetStackVoltage()
+        System.Console.WriteLine("Stack Voltage: " + (stackVoltage/1000.0).ToString() + " V")
 
-        System.Console.WriteLine("Stack Voltage: " + (voltage/1000.0).ToString() + " V")
-        System.Console.WriteLine("Stack Current: " + (current/1000.0).ToString() + " A")
+        ' Get current stack current (unit is mA)
+        Dim stackCurrent As Integer = master.GetStackCurrent()
+        System.Console.WriteLine("Stack Current: " + (stackCurrent/1000.0).ToString() + " A")
 
         System.Console.WriteLine("Press key to exit")
         System.Console.ReadLine()
