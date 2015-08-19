f73b59a514d1bd2e1348b7d028814b2d004c3e6a
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 15:33:44 +0200

    Cleanup Perl example

--- software/examples/perl/example_stack_status.pl
+++ software/examples/perl/example_stack_status.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickMaster;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => '6CPWYS'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 my $master = Tinkerforge::BrickMaster->new(&UID, $ipcon); # Create device object
@@ -13,12 +13,13 @@ my $master = Tinkerforge::BrickMaster->new(&UID, $ipcon); # Create device object
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
-# Get voltage and current from stack (in mV/mA)
-my $voltage = $master->get_stack_voltage()/1000.0;
-print "Stack Voltage: $voltage V\n";
+# Get current stack voltage (unit is mV)
+my $stack_voltage = $master->get_stack_voltage();
+print "Stack Voltage: " . $stack_voltage/1000.0 . " V\n";
 
-my $current = $master->get_stack_current()/1000.0;
-print "Stack Current: $current A\n";
+# Get current stack current (unit is mA)
+my $stack_current = $master->get_stack_current();
+print "Stack Current: " . $stack_current/1000.0 . " A\n";
 
 print "Press any key to exit...\n";
 <STDIN>;
