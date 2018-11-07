428865557195865ce9dee845b20041e9981b8e54
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:24:24 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_stack_status.rs
+++ software/examples/rust/example_stack_status.rs
@@ -1,25 +1,25 @@
 use std::{error::Error, io};
 
-use tinkerforge::{ipconnection::IpConnection, master_brick::*};
+use tinkerforge::{ip_connection::IpConnection, master_brick::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XXYYZZ"; // Change XXYYZZ to the UID of your Master Brick
+const UID: &str = "XXYYZZ"; // Change XXYYZZ to the UID of your Master Brick.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let master_brick = MasterBrick::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let master = MasterBrick::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current stack voltage
-    let stack_voltage = master_brick.get_stack_voltage().recv()?;
-    println!("Stack Voltage: {}{}", stack_voltage as f32 / 1000.0, " V");
+    // Get current stack voltage.
+    let stack_voltage = master.get_stack_voltage().recv()?;
+    println!("Stack Voltage: {} V", stack_voltage as f32 / 1000.0);
 
-    // Get current stack current
-    let stack_current = master_brick.get_stack_current().recv()?;
-    println!("Stack Current: {}{}", stack_current as f32 / 1000.0, " A");
+    // Get current stack current.
+    let stack_current = master.get_stack_current().recv()?;
+    println!("Stack Current: {} A", stack_current as f32 / 1000.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
